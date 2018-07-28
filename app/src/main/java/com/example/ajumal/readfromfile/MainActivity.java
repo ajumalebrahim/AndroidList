132027de6fe64ac3f6bd77c33548f23bcd5db8ab
Ajumal <aebrahim@talabat.com>
2018-07-28 15:12:21 +0300

    first commit

---
+++ app/src/main/java/com/example/ajumal/readfromfile/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ajumal.readfromfile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    private String[] values = {
+            "one", "two", "three"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.List);
+        ;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String wrd = parent.getItemAtPosition(position).toString();
+//                String word = values[position];
+                Toast.makeText(MainActivity.this, wrd, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
